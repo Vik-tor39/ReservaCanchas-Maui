642babb3a2a310bba7957216f1f3dab4fe236104
Josue Riera <josueriera23@hotmail.com>
2025-01-26 15:51:32 -0500

    Creación de interfaz para conectarse con la API

---
+++ ReservaCanchasApp/Interfaces/IAPIRepository.cs
@@ -0,0 +1,26 @@
+﻿using ReservaCanchasApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaCanchasApp.Interfaces
+{
+    public interface IAPIRepository
+    {
+        public bool AgregarComplejo(Complejo complejo);
+        public bool AgregarCancha(Cancha cancha);
+        public bool AgregarReserva(Reserva reserva);
+        public bool AgregarUsuario(Usuario usuario);
+        public bool EliminarComplejo(int idComplejo);
+        public bool EliminarCancha(int idCancha);
+        public bool EliminarReserva(int idReserva);
+        public bool EliminarUsuario(int idUsuario);
+        public bool ModificarComplejo(int idComplejo, Complejo complejo);
+        public bool ModificarCancha(int idCancha, Cancha cancha);
+        public bool ModificarReserva(int idReserva, Reserva reserva);
+        public bool ModificarUsuario(int idUsuario, Usuario usuario);
+
+    }
+}
--- ReservaCanchasApp/Repositories/APIRepository.cs
+++ ReservaCanchasApp/Repositories/APIRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReservaCanchasApp.Interfaces;
+using ReservaCanchasApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,68 @@ using System.Threading.Tasks;
 
 namespace ReservaCanchasApp.Repositories
 {
-    internal class APIRepository
+    internal class APIRepository : IAPIRepository
     {
         public APIRepository() { }
 
+        public bool AgregarCancha(Cancha cancha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AgregarComplejo(Complejo complejo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AgregarReserva(Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AgregarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarCancha(int idCancha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarComplejo(int idComplejo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarReserva(int idReserva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarUsuario(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModificarCancha(int idCancha, Cancha cancha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModificarComplejo(int idComplejo, Complejo complejo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModificarReserva(int idReserva, Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModificarUsuario(int idUsuario, Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
